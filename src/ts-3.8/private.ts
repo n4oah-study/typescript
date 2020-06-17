36a38e1a35fd7db1d7005e2fb29aa145b118ead8
n4oah <n4oahdev@gmail.com>
2020-06-17 14:53:58 +0900

    private

--- src/ts-3.8/private.ts
+++ src/ts-3.8/private.ts
@@ -1,8 +1,8 @@
 // https://github.com/tc39/proposal-class-fields/
 
 class Person {
-  private name: string
-  #age: number
+  private name: string // Typescript의 private 키워드 활용하여 (컴파일 / 코딩 중에만 검사 됨)
+  #age: number // ES6의 # 으로 private 활용 (런타임 시에도 검사 됨)
   
   constructor(name: string, age: number) {
     this.name = name;
